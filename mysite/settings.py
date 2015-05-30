2aa8098e12f5fe3f3eacb5d49f5396919b6ced65
Michael Nickey <mnickey@gmail.com>
2015-05-30 12:01:07 -0700

    initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
+    'markdown-deux',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/Users/MNickey/PycharmProjects/django_test/mysite/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
